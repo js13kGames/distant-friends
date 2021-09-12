bac38b2bb7872827e5cd1acda7391a24c76367fa
Slash <slash@slashware.net>
2021-09-11 22:03:49 -0500

    More responsiveness

--- src/game.js
+++ src/game.js
@@ -55,9 +55,9 @@ function startGame() {
 
   setTimeout(async () => {
     await showConversation (makeAnimal('gato'), [
-      "I'm getting close to Ceres, I really want to see my friends again!",
-      "The panel in the top records their last known location",
-      "Using [Enter] I can change the waypoint for the compass at the bottom",
+      "I'm getting close to Ceres, can't wait to see my friends again!",
+      "The panel in the top shows their last known location",
+      ve()+ " to change the waypoint for the compass at the bottom",
       "Let's fly into Ceres and land into a city!"
     ]);
     contextHint('Land on a city in Ceres');
--- src/ui.js
+++ src/ui.js
@@ -2,6 +2,8 @@ friendNames = ['Kori', 'Ponchi', 'Patty'];
 friendHints = ['Creta', 'Cape Santos', 'Los Pinos'];
 fws = [];
 
+const ve = () => mo ? 'Tap' : 'Enter'; 
+
 function wrapText(txt) {
   wi = Math.floor((W - 270) / ctx.measureText('m').width);
   y = H - (mo ? 220 : 150);
@@ -28,7 +30,7 @@ function renderUI(c,d) {
     c.fillStyle= "#00ff00";
     c.fillText("Loading...",10,50);
     if (musicLoaded) {
-      c.fillText("Tap or Enter to start",10,70);
+      c.fillText(ve()+ " to start",10,70);
     }
   } else if (gState == 1) {
     c.font = font(48);
@@ -37,7 +39,7 @@ function renderUI(c,d) {
     c.fillText("Distanced Friends",W/2,50);
     c.font = font(20);
     c.fillStyle= "#ea0a8e";
-    c.fillText("Tap or Enter to start",W/2,290);
+    c.fillText(ve()+ " to start",W/2,290);
     c.fillStyle= "#ffffff";
     c.font = font(18);
     c.fillText("Programmed by Slashie",W/2,330);
@@ -87,7 +89,7 @@ function renderUI(c,d) {
     Renderer.renderShapes(c, SHAPES.triangle, W / 2, H - 60, 1, 1, angle, 50, 50, undefined, true);
     c.fillStyle= "#ffffff";
     c.fillText("to "+currentWaypoint.name+": " + Math.floor(rdist(p1, currentWaypoint) - currentWaypoint.size), W / 2, H - 20);
-    c.fillText("[Enter] to change", W / 2, H - 5);
+    c.fillText(ve()+ " to change", W / 2, H - 5);
     c.textAlign="left"; 
     if (mo && gState != 10) {
       low = H - 200;
@@ -121,7 +123,7 @@ function renderUI(c,d) {
     c.textAlign="left"; 
     wrapText(conversationText);
     c.textAlign="right"; 
-    c.fillText("[Press Enter]", W - 20, H - 65);
+    c.fillText("["+ve()+"]", W - 20, H - 65);
   }
 }
 
